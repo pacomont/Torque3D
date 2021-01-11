17a72122a856c18ba3d79f8fed26a5f21847cbea
Francisco Montañés <fco.gmon@gmail.com>
2021-01-11 19:17:16 +0100

    cambios varios

--- My Projects/SimTerraGen/game/art/terrains/materials.cs
+++ My Projects/SimTerraGen/game/art/terrains/materials.cs
@@ -20,3 +20,214 @@
 // IN THE SOFTWARE.
 //-----------------------------------------------------------------------------
 
+
+
+
+
+
+
+new TerrainMaterial()
+{
+   diffuseMap = "D:/ACART/2020/Terrenos/texture";
+   diffuseSize = "65536";
+   internalName = "newMaterialTerreno";
+   macroMap = "../../../../../ACART/2020/Terrenos/texture.ecw";
+};
+
+/*new TerrainMaterial()
+{
+   diffuseMap = "art/terrains/texture.png";
+   diffuseSize = "65536";
+   internalName = "theTerrainBaseMat";
+};*/
+
+new TerrainMaterial()
+{
+   diffuseMap = "D:/ACART/2020/Terrenos/texture";
+   diffuseSize = "65536";
+   internalName = "theTerrainBaseMat0";
+};
+
+new TerrainMaterial()
+{
+   diffuseMap = "D:/ACART/2020/Terrenos/texture";
+   diffuseSize = "65536";
+   internalName = "theTerrainBaseMat1";
+};
+
+new TerrainMaterial()
+{
+   diffuseMap = "D:/ACART/2020/Terrenos/texture.png";
+   diffuseSize = "65536";
+   internalName = "theTerrainBaseMat2";
+};
+
+new TerrainMaterial()
+{
+   diffuseMap = "art/terains/texture.png";
+   diffuseSize = "65536";
+   internalName = "newMaterial";
+};
+
+
+new TerrainMaterial()
+{
+   diffuseMap = "art/terrains/texture.png";
+   diffuseSize = "65536";
+   internalName = "theTerrainBaseMat";
+};
+
+new TerrainMaterial()
+{
+   diffuseMap = "D:/ACART/2020/Saharani/Ortoimagen/textura2.png";
+   diffuseSize = "20480";
+   internalName = "theTerrainBaseMat3";
+};
+
+new TerrainMaterial()
+{
+   diffuseMap = "D:/ACART/2020/Saharani/Ortoimagen/textura2.png";
+   diffuseSize = "20480";
+   internalName = "theTerrainBaseMat4";
+};
+
+new TerrainMaterial()
+{
+   diffuseMap = "DATA/Saharani/Ortoimagen/textura2.png";
+   diffuseSize = "20480";
+   internalName = "theTerrainBaseMat5";
+};
+
+new TerrainMaterial()
+{
+   diffuseMap = "art/terrains/textura2.png";
+   diffuseSize = "20480";
+   internalName = "theTerrainBaseMat6";
+};
+
+new TerrainMaterial()
+{
+   diffuseMap = "DATA/Saharani/Ortoimagen/textura2.png";
+   diffuseSize = "20480";
+   internalName = "theTerrainBaseMat7";
+};
+
+new TerrainMaterial()
+{
+   diffuseMap = "art/terrains/textura2.png";
+   diffuseSize = "20480";
+   internalName = "theTerrainBaseMat8";
+};
+
+new TerrainMaterial()
+{
+   diffuseMap = "DATA/Saharani/Ortoimagen/textura2.png";
+   diffuseSize = "20480";
+   internalName = "theTerrainBaseMat9";
+};
+
+new TerrainMaterial()
+{
+   diffuseMap = "DATA/Saharani/Ortoimagen/textura2.png";
+   diffuseSize = "20480";
+   internalName = "theTerrainBaseMat10";
+};
+
+new TerrainMaterial()
+{
+   diffuseMap = "DATA/Saharani/Ortoimagen/textura2.png";
+   diffuseSize = "20480";
+   internalName = "theTerrainBaseMat11";
+};
+
+new TerrainMaterial()
+{
+   diffuseMap = "DATA/Saharani/Ortoimagen/textura2.png";
+   diffuseSize = "20480";
+   internalName = "theTerrainBaseMat12";
+};
+
+new TerrainMaterial()
+{
+   diffuseMap = "DATA/Saharani/Ortoimagen/textura2.png";
+   diffuseSize = "20480";
+   internalName = "theTerrainBaseMat13";
+};
+
+new TerrainMaterial()
+{
+   diffuseMap = "DATA/Saharani/Ortoimagen/textura2.png";
+   diffuseSize = "20480";
+   internalName = "theTerrainBaseMat14";
+};
+
+new TerrainMaterial()
+{
+   diffuseMap = "DATA/Saharani/Ortoimagen/textura2.png";
+   diffuseSize = "20480";
+   internalName = "theTerrainBaseMat15";
+};
+
+new TerrainMaterial()
+{
+   diffuseMap = "DATA/Saharani/Ortoimagen/textura2.png";
+   diffuseSize = "20480";
+   internalName = "theTerrainBaseMat16";
+};
+
+new TerrainMaterial()
+{
+   diffuseMap = "DATA/Saharani/Ortoimagen/textura2.png";
+   diffuseSize = "20480";
+   internalName = "theTerrainBaseMat17";
+};
+
+new TerrainMaterial()
+{
+   diffuseMap = "D:/Repositorios/Torque3D/My Projects/SimTerraGen/data/CMT_San_Gregorio/Ortoimagen/CMT_San_Gregorio_8Km_Orto_5m.tif";
+   diffuseSize = "8005";
+   internalName = "theTerrainBaseMat18";
+};
+
+new TerrainMaterial()
+{
+   diffuseMap = "DATA/CMT_San_Gregorio/Ortoimagen/CMT_San_Gregorio_8Km_Orto_5m.png";
+   diffuseSize = "7577.6";
+   internalName = "theTerrainBaseMat19";
+};
+
+new TerrainMaterial()
+{
+   diffuseMap = "DATA/CMT_San_Gregorio/Ortoimagen/CMT_San_Gregorio_8Km_Orto_5m.png";
+   diffuseSize = "7577.6";
+   internalName = "theTerrainBaseMat20";
+};
+
+new TerrainMaterial()
+{
+   diffuseMap = "DATA/CMT_San_Gregorio/Ortoimagen/CMT_San_Gregorio_8Km_Orto_5m.png";
+   diffuseSize = "7577.6";
+   internalName = "theTerrainBaseMat21";
+};
+
+new TerrainMaterial()
+{
+   diffuseMap = "art/terrains/warnmat";
+   detailMap = "core/art/warnmat";
+   macroMap = "core/art/warnmat";
+   internalName = "warning_material";
+};
+
+new TerrainMaterial()
+{
+   diffuseMap = "art/terrains/CMT_Alvarez_de_Sotomayor_Orto_1m.png";
+   diffuseSize = "16384";
+   internalName = "theTerrainBaseMat22";
+};
+
+new TerrainMaterial()
+{
+   diffuseMap = "DATA/CMT_San_Gregorio/Ortoimagen/CMT_San_Gregorio_64Km_Orto_5m_16384.png";
+   diffuseSize = "16384";
+   internalName = "theTerrainBaseMat23";
+};
